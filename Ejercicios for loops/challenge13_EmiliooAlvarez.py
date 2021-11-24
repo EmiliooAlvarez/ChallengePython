b611b0fb915a42abd7fcf09538ae423fb3c62557
EmiliooAlvarez <alvarezalonsoemilio27@gmail.com>
2021-11-24 13:18:50 +0100

    Ejercicios for loops

--- Ejercicios for loops/challenge13_EmiliooAlvarez.py
+++ Ejercicios for loops/challenge13_EmiliooAlvarez.py
@@ -1,11 +1,22 @@
+from math import factorial
 print('Welcome to the Factorial Calculator App')
 num=int(input('What number would you like to compute the factorial of?:'))
 
-def fact_1(n):
-    factorial_total = 1
-    while n > 1:
-        factorial_total *= n
-        n -= 1
-        print(n)
-    return factorial_total
-print(num)
+
+def factorial(num): 
+    if num < 0: 
+        print("Factorial of negative num does not exist")
+
+    elif num == 0: 
+        return 1
+        
+    else: 
+        fact = 1
+        while(num > 1): 
+            fact *= num 
+            num -= 1
+        return fact 
+
+num = 5; 
+
+print("Factorial of",num,"is", factorial(num)) 
