5bd9c8e712be5040019f452c7621be12f5ef3b6e
EmiliooAlvarez <alvarezalonsoemilio27@gmail.com>
2021-12-14 13:32:42 +0100

    practica final

---
+++ practica final/proyectofinal__EmiliooAlvarez.py
@@ -0,0 +1,5 @@
+print('Welcome to the word guesser!!')
+name= input('Please enter your name:')
+print(f' Welcome to the word guesser {name} !')
+
+print('Guess a 7 letter word from the following category: Classes ')
